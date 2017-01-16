6ead20445bb717e27b97affd09670543d1b0fb4a
Jackxsj <xshengjin@gmail.com>
2017-01-16 18:22:13 +0800

    the img link is ok, big problem with js issue

--- zhihu_detail/zhihu_detail/spiders/zhihu_detail_spider.py
+++ zhihu_detail/zhihu_detail/spiders/zhihu_detail_spider.py
@@ -32,17 +32,32 @@ class ZhihuDetailSpider(CrawlSpider):
         #使用//注意是从根节点开始定义，这里已经用sel过滤一次后，内循环不需要/
         for sel in response.xpath('//div[@class="zm-item-answer  zm-item-expanded"]'):
             item = ZhihuDetailItem()
+
+
+             #匿名用户处理
+            tmp_p = ''
+            tmp_p2 = ''
+            
+            if sel.xpath('div/div/span[@class="name"]'):
+                tmp_p = sel.xpath('div/div[@class="zm-item-answer-author-info"]/span[@class="name"]/text()').extract()
+                #tmp_p = sel_tmp.xpath('span[@class="name"]/text()').extract()
+                print tmp_p
+
+            if sel.xpath('div[@class="answer-head"]/div[@class="zm-item-answer-author-info"]/a[@class="zm-item-link-avatar avatar-link"]'):
+                print 'checking tmp_p2'
+                tmp_p2 = sel.xpath('div/div/a[@class="zm-item-link-avatar avatar-link"]/@href').extract()
+                #tmp_p2 = sel.xpath('div/div/span[@class="summary-wrapper"]/span/a[@class="author-link"]/@href').extract()
+                print tmp_p2
+                
             item['qid'] = tmp_qid
             print item['qid']
             item['aid'] = (sel.xpath('@data-atoken').extract())[0]
             print item['aid']
             item['zan'] = (sel.xpath('div/button/span[@class="count"]/text()').extract())[0]
             print item['zan']
-            #匿名用户处理
-            tmp_p = sel.xpath('div/div[@class="zm-item-answer-author-info"]/span[@class="name"]/text()').extract()
-            print tmp_p
-            tmp_p2 = sel.xpath('div/div[@class="zm-item-answer-author-info"]/span[@class="summary-wrapper"]/span/a/@href').extract()
-            print tmp_p2
+           
+
+                    
             if tmp_p :
                 item['people'] = tmp_p[0]
             elif tmp_p2:
@@ -60,15 +75,20 @@ class ZhihuDetailSpider(CrawlSpider):
             #TODO4: itemPipline写入
             #TODO5: 测试
             tmp_img_link = ''
-            for sel_img in sel.xpath('img'):
+            for sel_img in sel.xpath('div/div[@class="zm-editable-content clearfix"]/img'):
                 print 'check img'
-                tmp_img_link = tmp_img_link +';;'+(sel_img.xpath('@data-original').extract())[0]
-                print tmp_img_link
+                if sel_img.xpath('@data-original'):
+                    tmp_img_link = tmp_img_link +';;'+(sel_img.xpath('@data-original').extract())[0]
+            for sel_img in sel.xpath('div/div[@class="zm-editable-content clearfix"]/b/img'):
+                print 'check img in block'
+                if sel_img.xpath('@data-original'):
+                    tmp_img_link = tmp_img_link +';;'+(sel_img.xpath('@data-original').extract())[0]
+        
             item['imglink']=tmp_img_link
             print item['imglink']
             print '-' * 60
 
             # 赞大于zan_th的回答才是需要的
-#            if int(zan_str.group(1)) >= ZhihuDetailSpider.my_parse.zan_th:
-#                yield item
+         #   if int(item['zan']) >= ZhihuDetailSpider.my_parse.zan_th:
+        #        yield item
                 
